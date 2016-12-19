c0f18bf942443860e38b263878b27113570439af
bacchus <zukamimiridoari@gmail.com>
2016-12-20 01:48:22 +0200

    add dvd-test & rgbliink

---
+++ dvd-test1/dvd-test1.ino
@@ -0,0 +1,26 @@
+int i = 0;
+
+void setup() {                
+  pinMode(8, OUTPUT);     
+  pinMode(9, OUTPUT);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+}
+
+void loop() {
+  /*
+  digitalWrite(9, HIGH);
+  delay(1);          
+  digitalWrite(9, LOW); 
+  delay(1);
+
+  i++;
+
+  if (i==2000) {
+    digitalWrite(8, HIGH);
+  } else if (i==4000) {
+    i = 0;
+    digitalWrite(8, LOW);
+  }
+  */
+}
---
+++ rgblink/rgblink.ino
@@ -0,0 +1,47 @@
+// rgb test
+
+int rid = 10;
+int gid = 11;
+int bid = 12;
+
+void setup() {
+  pinMode(rid, OUTPUT);
+  pinMode(gid, OUTPUT);
+  pinMode(bid, OUTPUT);
+
+  off();
+}
+
+void off() {
+  digitalWrite(rid, LOW);
+  digitalWrite(gid, LOW);
+  digitalWrite(bid, LOW);
+}
+
+void xon(int x) {
+  off();
+  digitalWrite(x, HIGH);
+}
+
+void ron() { xon(rid); }
+void gon() { xon(gid); }
+void bon() { xon(bid); }
+
+void rgbon(float r, float g, float b) {
+  float sum = r + g + b;
+  float tm = 1000;
+  r *= tm/sum;
+  g *= tm/sum;
+  b *= tm/sum;
+  
+  ron(); delayMicroseconds(r);
+  gon(); delayMicroseconds(g);
+  bon(); delayMicroseconds(b);
+}
+
+void loop() {
+  //rgbon(1000, 100, 20);
+  off();
+  delay(2000);
+}
+
